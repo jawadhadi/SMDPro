e3b288c3477ddb40e24a82989ad950d7dc60ac22
Hadi <hadi@Hadis-MacBook-Pro.local>
2015-05-21 18:13:46 +0500

    Initial Commit

---
+++ iOSHacker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSHacker
+//
+//  Created by Hadi on 21/05/2015.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOSHacker/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iOSHacker
+//
+//  Created by Hadi on 21/05/2015.
+//
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

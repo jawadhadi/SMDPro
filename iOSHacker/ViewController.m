81cfd1a876a96e95f43b0f0e7d9653b6c668f7b6
jawadhadi <jhadi@me.com>
2015-05-23 02:20:59 +0500

    added code to parse JSON

--- iOSHacker/ViewController.m
+++ iOSHacker/ViewController.m
@@ -10,6 +10,8 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) NSArray *posts;
+
 @end
 
 @implementation ViewController
@@ -20,6 +22,38 @@
     NSLog(@"hello, world");
     
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //code to fetch JSON data and parse it to foundation objects
+    
+    NSURL *blogURL = [NSURL URLWithString:@"http://ioshacker.com/?json=1"];
+    
+    NSData *jsonData = [NSData dataWithContentsOfURL:blogURL];
+    
+    NSError *error = nil;
+    
+    NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:jsonData options:0 error:&error];
+    
+    NSLog(@"1");
+    
+    self.posts = [dataDictionary objectForKey:@"posts"];
+    
+    //NSLog(@"%@", dataDictionary);
+    
+    
+    //NSLog(@"%@", posts);
+    
+    //NSArray* post1 = [posts objectForKey:@"title"];
+    
+    NSString *title = [[self.posts objectAtIndex:0]valueForKey:@"title_plain"];
+    NSString *date = [[self.posts objectAtIndex:0]valueForKey:@"date"];
+    
+    NSString *content = [[self.posts objectAtIndex:0]valueForKey:@"content"];
+    
+    
+    NSLog(@"%@", title);
+    NSLog(@"%@", date);
+    NSLog(@"%@", content);
+    
 }
 
 - (void)didReceiveMemoryWarning {

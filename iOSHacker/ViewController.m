835a32fdb103dbb1e088c278bc40dde489b26678
jawadhadi <jhadi@me.com>
2015-05-24 22:24:41 +0500

    added UITableView

--- iOSHacker/ViewController.m
+++ iOSHacker/ViewController.m
@@ -33,7 +33,7 @@
     
     NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:jsonData options:0 error:&error];
     
-    NSLog(@"1");
+    //NSLog(@"1");
     
     self.posts = [dataDictionary objectForKey:@"posts"];
     
@@ -47,7 +47,7 @@
     NSString *title = [[self.posts objectAtIndex:0]valueForKey:@"title_plain"];
     NSString *date = [[self.posts objectAtIndex:0]valueForKey:@"date"];
     
-    NSString *content = [[self.posts objectAtIndex:0]valueForKey:@"content"];
+    NSString *content = [[self.posts objectAtIndex:0]valueForKey:@"excerpt"];
     
     
     NSLog(@"%@", title);
@@ -59,6 +59,38 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    return self.posts.count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    NSString *identifier = @"post";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+    }
+    
+    cell.textLabel.text = [[self.posts objectAtIndex:indexPath.row]valueForKey:@"title_plain"];
+ 
+    return cell;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+//    if (indexPath.section == 0) {
+//        if (indexPath.row == 1) {
+//            return 120;
+//        }
+//    }
+    return 100;
+    
 }
 
 @end

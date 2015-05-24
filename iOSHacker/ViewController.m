b6cadec1132a90401bac524c2c899f013bcb166c
jawadhadi <jhadi@me.com>
2015-05-25 00:58:43 +0500

    UITableView layout fixed (sort of)

--- iOSHacker/ViewController.m
+++ iOSHacker/ViewController.m
@@ -11,6 +11,7 @@
 @interface ViewController ()
 
 @property (nonatomic, strong) NSArray *posts;
+@property (nonatomic, strong) NSArray *attachments;
 
 @end
 
@@ -37,7 +38,14 @@
     
     self.posts = [dataDictionary objectForKey:@"posts"];
     
-    //NSLog(@"%@", dataDictionary);
+    
+    self.attachments = [[self.posts objectAtIndex:0]valueForKey:@"attachments"];
+    
+    //NSString *img = [[self.attachments objectAtIndex:0]valueForKey:@"url"];
+    
+    
+    
+    //NSLog(@"%@", img);
     
     
     //NSLog(@"%@", posts);
@@ -75,9 +83,34 @@
     
     if (cell == nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+        
+        cell.textLabel.numberOfLines = 0;
+        cell.textLabel.font = [UIFont systemFontOfSize:16.0];
+        cell.textLabel.lineBreakMode = NSLineBreakByWordWrapping;
     }
     
     cell.textLabel.text = [[self.posts objectAtIndex:indexPath.row]valueForKey:@"title_plain"];
+    
+    //cell.imageView.image = [[self.attachments objectAtIndex:indexPath.row]valueForKey:@"url"];
+    
+    
+//    NSURL *imageURL = [NSURL URLWithString:@"http://d1mxp0yvealdwc.cloudfront.net/e92c939d-e83b-4592-b367-327fa67339fb/1001 123.jpg"];
+//    NSLog(@"imge %@",imageURL);
+    
+//    NSData *imageData = [NSData dataWithContentsOfURL:[[self.attachments objectAtIndex:0]valueForKey:@"url"]];
+//    //NSLog(@"imge %@",imageData);
+//    UIImage *image = [UIImage imageWithData:imageData];
+//    //NSLog(@"imge %@",image);
+//    cell.imageView.image = image;
+
+    
+//    NSURL *url = [[self.attachments objectAtIndex:0]valueForKey:@"url"];
+//    NSData *data = [NSData dataWithContentsOfURL:url];
+//    UIImage *img = [[UIImage alloc] initWithData:data];
+//    
+//    NSLog(@"%@", img);
+//    cell.imageView.image = img;
+    
  
     return cell;
 }
@@ -89,8 +122,32 @@
 //            return 120;
 //        }
 //    }
-    return 100;
+    
+//    CGSize rowSize=[self calculateLabelHeightWith:320 text:[[self.posts objectAtIndex:indexPath.row]valueForKey:@"title_plain"]];
+//    
+//    return (rowSize.height+20);
+    
+    return 80;
     
 }
+
+
+-  (CGSize) calculateLabelHeightWith:(CGFloat)width text:(NSString*)textString
+{
+    CGSize maximumSize = CGSizeMake(width, 1000);
+//    CGSize size = [textString sizeWithFont:[UIFont fontWithName:@"HelveticaNeue-Medium" size:14]
+//                         constrainedToSize:maximumSize
+//                             lineBreakMode:UILineBreakModeWordWrap];
+    
+    CGRect textRect = [textString boundingRectWithSize:maximumSize
+                                         options:NSStringDrawingUsesLineFragmentOrigin
+                                      attributes:@{NSFontAttributeName:[UIFont fontWithName:@"HelveticaNeue-Medium" size:18]}
+                                         context:nil];
+    
+    //CGSize size = textRect.size;
+    
+    return textRect.size;
+}
+
 
 @end
